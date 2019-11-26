7c0bdad42f254d5c093c487129c98ea4114e4873
strawbee <yuejiao.hou@gmail.com>
2019-11-26 11:32:42 -0800

    Initial setup and problem

---
+++ 0026_remove_duplicates.js
@@ -0,0 +1,34 @@
+/**
+ * Remove Duplicates from Sorted Array
+ * @url https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * @difficulty easy
+ *
+ * @input sorted array
+ * @output array length
+ *
+ * Remove duplicates in place. Elements beyond the returned length are ignored.
+ */
+
+function removeDuplicates(nums) {
+  if (nums.length <= 1) return nums.length;
+
+  let current_idx = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if(nums[i] !== nums[i - 1]) {
+      nums[current_idx] = nums[i];
+      current_idx++;
+    }
+  }
+  return current_idx;
+}
+
+/**
+ * COMMENTS
+ * current_idx is a pointer to the current index of the array without
+ * duplicates.
+ *
+ * @time_complexity O(n). At most we traverse the nums array once, if no
+ * duplicates exist.
+ * @space_complexity O(1). We take up only constant extra space that does not
+ * increase depending on the size of the input.
+ */
